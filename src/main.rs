aaf904022df5bd300ee852585acdd09baaba550c
Alexander Rao <alexhrao@gmail.com>
2022-12-18 22:02:26 -0600

    Rename to two digits

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,60 @@
-mod day1;
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
 mod day10;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
+mod day11;
 mod utils;
 fn main() {
     match std::env::args().last().unwrap().parse::<u8>().unwrap() {
         1 => {
-            day1::task1();
-            day1::task2();
+            day01::task1();
+            day01::task2();
         }
         2 => {
-            day2::task1();
-            day2::task2();
+            day02::task1();
+            day02::task2();
         }
         3 => {
-            day3::task1();
-            day3::task2();
+            day03::task1();
+            day03::task2();
         }
         4 => {
-            day4::task1();
-            day4::task2();
+            day04::task1();
+            day04::task2();
         }
         5 => {
-            day5::task1();
-            day5::task2();
+            day05::task1();
+            day05::task2();
         }
         6 => {
-            day6::task1();
-            day6::task2();
+            day06::task1();
+            day06::task2();
         }
         7 => {
-            day7::task1();
-            day7::task2();
+            day07::task1();
+            day07::task2();
         }
         8 => {
-            day8::task1();
-            day8::task2();
+            day08::task1();
+            day08::task2();
         }
         9 => {
-            day9::task1();
-            day9::task2();
+            day09::task1();
+            day09::task2();
         }
         10 => {
             day10::task1();
             day10::task2();
+        }
+        11 => {
+            day11::task1();
+            day11::task2();
         }
         x => todo!("Day {}", x),
     }
--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,7 @@
 pub fn get_input(day: u8) -> String {
     unsafe {
         std::string::String::from_utf8_unchecked(
-            std::fs::read(format!("resources/day{}input.txt", day)).unwrap(),
+            std::fs::read(format!("resources/day{:02}input.txt", day)).unwrap(),
         )
     }
 }
